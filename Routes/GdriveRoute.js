89f5a2a23f2a47231a3876a28652bb74a60a9571
MohamedKaif-ft <141742064+MohamedKaif-ft@users.noreply.github.com>
2023-12-27 22:06:25 +0530

    updated dgrive route

--- Routes/GdriveRoute.js
+++ Routes/GdriveRoute.js
@@ -12,56 +12,53 @@ const verifyToken = require('../Authorization/verifyToken');
 const upload = multer({ storage: storage });
 
 
-router.post('/screenshot',  upload.single('image'), async (req, res) => {
-    console.log(req.file); 
-    console.log(req.body);
-  
-    try {
-      if (!req.file) {
-        return res.status(400).json({ error: 'No file uploaded.' });
-      }
-  
-      const userId = 17;
-      const imageName = req.file.originalname;
-      const serverFilePath = path.join(__dirname, '../images', imageName);
-  
-      fs.writeFileSync(serverFilePath, req.file.buffer);
-  
-      const fileMetadata = {
-        name: imageName,
-        parents: ['1Sp7UVR3bQ0PfGoFQz7AWVEmC_ruzwAmp'],
-      };
-  
-      const bufferStream = new stream.PassThrough();
-      bufferStream.end(req.file.buffer);
-  
-      const media = {
-        mimeType: req.file.mimetype,
-        body: bufferStream,
-      };
-  
-      const driveRes = await drive.files.create({
-        resource: fileMetadata,
-        media: media,
-        fields: 'id',
-      });
-      console.log('Google Drive API Request:', {
-        resource: fileMetadata,
-        media: media,
-        fields: 'id',
-      });
-  
-      console.log('Google Drive API Response:', driveRes);
-      const imageId = driveRes.data.id;
-      await addImage(userId, imageName, imageId);
-      fs.unlinkSync(serverFilePath);
-  
-      res.json({ success: true, message: 'Image uploaded to Google Drive', imageId });
-      res.status(200);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
+router.post('/screenshot', upload.single('image'),verifyToken, async (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded.' });
     }
-  });
+
+    const userId = req.user.id;
+    const imageName = req.file.originalname;
+
+    const fileMetadata = {
+      name: imageName,
+      parents: ['1Sp7UVR3bQ0PfGoFQz7AWVEmC_ruzwAmp'],
+    };
+
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(req.file.buffer);
+
+    const media = {
+      mimeType: req.file.mimetype,
+      body: bufferStream,
+    };
+
+    const driveRes = await drive.files.create({
+      resource: fileMetadata,
+      media: media,
+      fields: 'id',
+    });
+
+    console.log('Google Drive API Request:', {
+      resource: fileMetadata,
+      media: media,
+      fields: 'id',
+    });
+
+    console.log('Google Drive API Response:', driveRes);
+    const imageId = driveRes.data.id;
+    await addImage(userId, imageName, imageId);
+
+    res.json({ success: true, message: 'Image uploaded to Google Drive', imageId });
+    res.status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
   
 module.exports = router;
