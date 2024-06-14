59e79d0c2f5994aacc88e527420b383da0d1d288
jais_vicky <ce21btech11045@iith.ac.in>
2024-06-14 10:14:20 +0530

    added postgresql using connectionstring

--- Config/dbConnection.js
+++ Config/dbConnection.js
@@ -1,20 +1,22 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 const fs = require('fs');
 
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL_URL + "?sslmode=require",
-// })
-    const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'forsky',
-    password: 'Mohamed2004',
-    port: 5432,
-  });
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+  //   const pool = new Pool({
+  //   user: 'tayog_user',
+  //   host: 'dpg-cpjb5gi1hbls73bt1ec0-a',
+  //   database: 'tayog',
+  //   password: 'OPQ2lA7aV3RTtHOQw1LnA4MNBejZO3KS',
+  //   port: 5432,
+  // });
 
-  pool.on('error', (err) => {
-    console.error('Database connection error:', err);
-    process.exit(-1);
-  });
+  // pool.on('error', (err) => {
+  //   console.error('Database connection error:', err);
+  //   process.exit(-1);
+  // });
   
   module.exports = pool;
