bb23fb4b9a40a400f12adf9d4d31e8f5204c1f24
jais_vicky <ce21btech11045@iith.ac.in>
2024-06-14 10:13:36 +0530

    added role-update route

--- Routes/put_router.js
+++ Routes/put_router.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const {updatePassword} = require('../functions/otp')
+const {updatePassword} = require('../functions/otp');
+const dynamicJwtCheck = require('../Authorization/dynamicVerifyToken');
+const pool = require('../Config/dbConnection');
 
 router.put('/update-password',updatePassword)
+
+router.put('/update-role-name', dynamicJwtCheck, async(req,res) =>{
+    try {
+        console.log(req.user)
+        const client = await pool.connect();
+        const email = req.user.email;
+        if(email !== req.body.email){
+            return res.status(400).json({message: "You are not the owner of this email"})
+        }
+    
+        const checkExit = await client.query("SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)", [email]);
+        if(!checkExit.rows[0].exists){
+            return res.status(400).json({message: "User doesn't exit"});
+        }
+    
+        const result = await client.query(`UPDATE users SET role = $1 WHERE email = $2`, [req.body.role, email]);
+        await client.release();
+        res.status(200).json({message: "successful updated"})
+    } catch (error) {
+        console.log(error.message);
+    }
+
+})
 
 module.exports = router
